6ce53098b5f1812d18dbbb879e95232e23fb4360
96berry96 <miltoncsedev@gmail.com>
2024-04-25 14:28:56 +0100

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-template-driven-form';
+
+  @ViewChild('registrationForm') 
+  form:NgForm;
+
+  firstName: string = '';
+  lastName: string = '';
+  emailAddress: string = '';
+
+  genders= [
+    { id:'check-male', value:'male', display: 'Male'},
+    { id:'check-female', value:'female', display:'Female'},
+    { id:'other', value:'other', display: 'Prefer not to say'}
+  ]
+
+  OnFormSubmitted(){
+    console.log(this.form);
+    
+    console.log(this.form.value);
+    
+    
+  }
 }
